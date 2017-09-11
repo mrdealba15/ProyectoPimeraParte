cb3f9c7d36dfd7863dc7633acda7499d9aebbc8e
William Llanos <williaml@uninorte.edu.co>
2017-09-11 16:25:26 -0500

    Fix1

--- app/src/main/java/com/uninorte/proyecto1/MainActivity.java
+++ app/src/main/java/com/uninorte/proyecto1/MainActivity.java
@@ -1,21 +1,32 @@
 package com.uninorte.proyecto1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private int iC = 1;
+    private int iR = 1;
+    private ViewGroup layoutC;
+    private ViewGroup layoutR;
+    private ViewFlipper vf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +34,27 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        vf = (ViewFlipper) findViewById(R.id.vf);
+        vf.setDisplayedChild(0);
+        layoutC = (ViewGroup) findViewById(R.id.contentC);
+        layoutR = (ViewGroup) findViewById(R.id.contentR);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                switch (vf.getDisplayedChild()) {
+                    case 0:
+                        //Rubrica
+                        iR++;
+                        addEl(R.layout.data_rubrica, "Rúbrica " + iR, layoutR);
+                        break;
+                    case 1:
+                        //Cursos
+                        iC++;
+                        addEl(R.layout.data_curso, "Curso " + iC, layoutC);
+                        break;
+                }
             }
         });
 
@@ -82,13 +107,11 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-
+            vf.setDisplayedChild(0);
 
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-            Intent i = new Intent(this,Cursos.class);
-            startActivity(i);
-
+            vf.setDisplayedChild(1);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -99,6 +122,22 @@ public class MainActivity extends AppCompatActivity
     public void onClickGoto(View view){
         Intent i = new Intent(this, Rubrica.class);
         startActivity(i);
+    }
+
+
+    public void onClickGotoCursos(View view) {
+        Intent i = new Intent(this, Curso.class);
+        startActivity(i);
+    }
+
+    void addEl(int id, String msg, ViewGroup layout) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        LinearLayout linearLayout = (LinearLayout) inflater.inflate(id, null, false);
+
+        TextView textView = (TextView) linearLayout.findViewById(R.id.textView1);
+        Button editText = (Button) linearLayout.findViewById(R.id.button1);
+        textView.setText(msg);
+        layout.addView(linearLayout);
     }
 
 
